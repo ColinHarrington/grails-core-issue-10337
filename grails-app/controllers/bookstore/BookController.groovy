64a0d50e0832a2365b7f31d2f2c3a0f0b615f80c
Colin Harrington <colin.harrington@gmail.com>
2016-11-29 21:32:06 -0600

    Formatting

--- grails-app/controllers/bookstore/BookController.groovy
+++ grails-app/controllers/bookstore/BookController.groovy
@@ -1,9 +1,9 @@
 package bookstore
 
 class BookController {
-	static responseFormats = ['json', 'xml']
-	
-    def index() { 
-    	render(view: 'book', model:[title: 'hobbit', author: 'J.R.R Tolkein'])
+    static responseFormats = ['json', 'xml']
+
+    def index() {
+        render(view: 'book', model: [title: 'hobbit', author: 'J.R.R Tolkein'])
     }
 }
